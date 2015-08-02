f76938da3b1cafcb97c8377b46a77c90a9370551
Yuri Fidélis Pinto <contato.yurifidelis@gmail.com>
2015-08-02 17:52:11 -0300

    Configs. iniciais de banco de dados

--- WMId/WMId/settings.py
+++ WMId/WMId/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'WMId.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_wmid',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock',
+        'PORT': '8889'
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
