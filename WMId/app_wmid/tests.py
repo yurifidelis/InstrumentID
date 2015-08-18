fd148145f50d8522656b6f6c391751cf82cf5045
Yuri Fidélis Pinto <contato.yurifidelis@gmail.com>
2015-08-17 22:18:18 -0300

    Testes iniciais para track,album,artist

--- WMId/app_wmid/tests.py
+++ WMId/app_wmid/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Track, Album, Artist
+
+def create_track(album, file, title, position, duration):
+    # Cria uma faixa com os atributos passados
+    return Track.objects.create(album=album, file=file, title=title, position=position, duration=duration)
+
+def create_album(artist, title, year):
+    # Cria uma faixa com os atributos passados
+    return Album.objects.create(artist=artist, title=title, year=year)
+
+def create_artist(name):
+    # Cria uma faixa com os atributos passados
+    return Artist.objects.create(name=name)
+
+# TODO: Testes para
+# Faixa não pode ser adicionada sem que haja um album na base de dados
+# Albums sem faixas podem ser excluidos de qualquer listagem
